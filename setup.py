3e73968046555a230425c95995f97d7c98ad57aa
Uznick <uznick@ya.ru>
2010-11-30 17:47:57 +0300

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='txStomper',
-      version='1.0.1',
+      version='1.0.2',
       description='STOMP client library for Twisted Python',
       author='Twingly AB',
       author_email='oskar@osd.se',
